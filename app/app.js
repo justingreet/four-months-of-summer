a317170764a8abc6e5657864d6c7ae3844482394
Justin Greet <justin.greet11@gmail.com>
2016-05-06 00:06:00 -0400

    Delete old stuff. Add new stuff.

--- app/app.js
+++ app/app.js
@@ -2,11 +2,20 @@
 
 // Declare app level module which depends on views, and components
 angular.module('myApp', [
-  'ngRoute',
-  'myApp.view1',
-  'myApp.view2',
-  'myApp.version'
+  'ngRoute'
 ]).
 config(['$routeProvider', function($routeProvider) {
-  $routeProvider.otherwise({redirectTo: '/view1'});
+  $routeProvider.when('/dates/:dateId', {
+    templateUrl: 'dates/date.html',
+    controller:,
+    controllerAs: 'ctrl',
+    resolve: {
+      day: getDay
+    }
+  });
+  $routeProvider.otherwise({redirectTo: '/dates/1'});
 }]);
+
+getDay = function($route) {
+  var dayId = $route.current.params.dateId;
+};
---
+++ app/dates/date.js
@@ -0,0 +1,3 @@
+/**
+ * Created by justin on 5/6/16.
+ */
